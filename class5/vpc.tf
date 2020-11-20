e7de569236040a2a521adcb7c899b593b62d9601
farrukh90 <farrukhsadykov@gmail.com>
2020-11-20 00:32:08 +0000

    added vpc

--- class5/vpc.tf
+++ class5/vpc.tf
@@ -1,33 +1,3 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
-
-resource "aws_subnet" "subnet1" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.1.0/24"
-}
-
-resource "aws_subnet" "subnet2" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.2.0/24"
-}
-
-resource "aws_subnet" "subnet3" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.3.0/24"
-}
-
-resource "aws_subnet" "subnet101" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.101.0/24"
-}
-
-resource "aws_subnet" "subnet102" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.102.0/24"
-}
-
-resource "aws_subnet" "subnet103" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.103.0/24"
-}
