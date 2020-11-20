9899c04c31dd63a7d71d0a7e490d8c008254b9e1
farrukh90 <farrukhsadykov@gmail.com>
2020-11-20 00:19:45 +0000

    added vpc

--- class5/sec_group.tf
+++ class5/sec_group.tf
@@ -1,6 +1,7 @@
 resource "aws_security_group" "vpc_task" {
   name        = "vpc_task"
   description = "Allow TLS inbound traffic"
+
   ingress {
     description = "Allow ssh"
     from_port   = 22
@@ -8,6 +9,7 @@ resource "aws_security_group" "vpc_task" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
