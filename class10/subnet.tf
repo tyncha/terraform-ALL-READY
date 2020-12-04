c61b13b0005431bb07aaef2c56e1c00748f8dc80
farrukh90 <farrukhsadykov@gmail.com>
2020-12-04 01:56:25 +0000

    added subnet

--- class10/subnet.tf
+++ class10/subnet.tf
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "private2" {
   name = "private2"
   resource_group_name = "${var.resource_group_name}"
   virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
-  address_prefix = "${var.private_subnet1_range}"
+  address_prefix = "${var.private_subnet2_range}"
 
 }
 
