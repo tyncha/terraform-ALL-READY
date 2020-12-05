211eb75a8da69137fcde7c01fcc49d1e284db668
farrukh90 <farrukhsadykov@gmail.com>
2020-12-05 21:41:05 +0000

    added dynamic code

--- class11/ss/test/regions/west_europe.tf
+++ class11/ss/test/regions/west_europe.tf
@@ -1,3 +1,10 @@
-  resource_group_name     = "example-resources"
-  resource_group_location = "West Europe"
-  ss_name                 = "example-ss"
+resource_group_name     = "example-resources"
+resource_group_location = "West Europe"
+ss_name                 = "example-ss"
+admin_username          = "admin"
+instances               =  1 
+sku                     = "Standard_F2"
+address_prefixes        = ["10.0.2.0/24"]
+subnet_name             = "subnet1"
+address_space           = ["10.0.0.0/16"]
+vpc_name                = "example1"
--- class11/ss/test/ss_module.tf
+++ class11/ss/test/ss_module.tf
@@ -3,4 +3,10 @@ module "ss" {
     resource_group_name     = var.resource_group_name
     resource_group_location = var.resource_group_location
     ss_name                 = var.ss_name
+    admin_username          = var.admin_username
+    instances               = var.instances
+    sku                     = var.sku
+    subnet_name             = var.subnet_name
+    address_space           = var.address_space
+    vpc_name                = var.vpc_name 
 }
