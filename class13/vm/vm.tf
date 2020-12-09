62bd0a9f11ffffa93090abb4e6c295f1321e1d7c
farrukh90 <farrukhsadykov@gmail.com>
2020-12-09 02:57:29 +0000

    added vpc

--- class13/vm/vm.tf
+++ class13/vm/vm.tf
@@ -1,20 +1,21 @@
 resource "google_compute_instance" "vm_instance" {
-	name = "terraform-instance"
-	machine_type = "f1-micro"
-	boot_disk {
-		initialize_params {
-		image = "debian-cloud/debian-9"
-		}
-	}
-	network_interface {
-		network = "default"
-		access_config {
-		}
-	}
-    metadata_startup_script = "${file("userdata.sh")}"
-    metadata = {
-		ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}sam:${file("~/.ssh/id_rsa.pub")}"
-	}
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
 
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  metadata_startup_script = "${file("userdata.sh")}"
+
+  metadata = {
+    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}sam:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
