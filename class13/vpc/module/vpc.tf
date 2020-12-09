2f1f3d586653a8c2689f99d8d3a057053f67d751
farrukh90 <farrukhsadykov@gmail.com>
2020-12-09 02:57:05 +0000

    added vpc module

--- class13/vpc/module/vpc.tf
+++ class13/vpc/module/vpc.tf
@@ -1,23 +1,23 @@
 resource "google_compute_network" "vpc" {
-  name                    = "vpc"
+  name                    = "${var.vpc_name}"
   auto_create_subnetworks = "false"
   routing_mode            = "GLOBAL"
 }
 
 resource "google_compute_subnetwork" "public_subnet_1" {
-  name          = "public-subnet1"
-  ip_cidr_range = "10.0.1.0/24"
+  name          = "${var.public-subnet1-name}"
+  ip_cidr_range = "${var.ip_cidr_range1}"
   network       = "${google_compute_network.vpc.name}"
 }
 
 resource "google_compute_subnetwork" "public_subnet_2" {
-  name          = "public-subnet2"
-  ip_cidr_range = "10.0.2.0/24"
+  name          = "${var.public-subnet2-name}"
+  ip_cidr_range = "${var.ip_cidr_range2}"
   network       = "${google_compute_network.vpc.name}"
 }
 
 resource "google_compute_subnetwork" "public_subnet_3" {
-  name          = "public-subnet3"
-  ip_cidr_range = "10.0.3.0/24"
+  name          = "${var.public-subnet3-name}"
+  ip_cidr_range = "${var.ip_cidr_range3}"
   network       = "${google_compute_network.vpc.name}"
 }
