36fe2b900b21af3707ac3bec9fb81143acf32060
farrukh90 <farrukhsadykov@gmail.com>
2020-12-11 00:59:18 +0000

    added variable for region

--- class14/asg.tf
+++ class14/asg.tf
@@ -1,13 +1,13 @@
 resource "google_compute_network" "vpc" {
-    project = var.project_id
+  project                 = var.project_id
   name                    = "vpc"
   auto_create_subnetworks = "false"
   routing_mode            = "GLOBAL"
 }
 
 resource "google_compute_subnetwork" "public_subnet_1" {
-    project = var.project_id
-  region      = "us-central1"
+  project = var.project_id
+  region = var.region
 
   name          = "public-subnet1"
   ip_cidr_range = "10.0.1.0/24"
@@ -16,6 +16,8 @@ resource "google_compute_subnetwork" "public_subnet_1" {
 
 
 resource "google_compute_autoscaler" "foobar" {
+  project = var.project_id
+
   name   = "my-autoscaler"
   zone   = "us-central1-f"
   target = google_compute_instance_group_manager.foobar.id
@@ -30,10 +32,8 @@ resource "google_compute_autoscaler" "foobar" {
 }
 
 
-
 resource "google_compute_instance_template" "foobar" {
-    project = var.project_id
-
+  project        = var.project_id
   name           = "my-instance-template"
   machine_type   = "e2-medium"
   can_ip_forward = false
@@ -42,22 +42,25 @@ resource "google_compute_instance_template" "foobar" {
     source_image = "debian-cloud/debian-9"
   }
   network_interface {
-    network = google_compute_subnetwork.public_subnet_1.name
+    network = "default"
   }
 }
 
 
-resource "google_compute_target_pool" "foobar" {
-    project = var.project_id
 
-  region  = "us-central1"
-  name    = "my-target-pool"
+resource "google_compute_target_pool" "foobar" {
+  project = var.project_id
+
+  region = var.region
+  name   = "my-target-pool"
 }
 
 
 resource "google_compute_instance_group_manager" "foobar" {
-  name    = "my-igm"
-  zone    = "us-central1-f"
+  project = var.project_id
+
+  name = "my-igm"
+  zone = "us-central1-f"
   version {
     instance_template = google_compute_instance_template.foobar.id
     name              = "primary"
--- class14/variable.tf
+++ class14/variable.tf
@@ -1,3 +1,6 @@
 variable "project_id" {
-    default = "crucial-haiku-298300"
+  default = "crucial-haiku-298300"
+}
+variable "region" {
+    default = "us-central1"
 }
